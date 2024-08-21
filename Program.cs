ef75f02bcac7b9797580f449b7de502453f5f4a6
MahBoiDeveloper <mah.boi.developer@gmail.com>
2024-08-21 21:26:59 +0300

    Add help info and rework arguments parsing

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 using System.Linq;
 
 namespace LangFSExporter
@@ -9,23 +8,111 @@ namespace LangFSExporter
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0) return;
-            
-            foreach (var arg in args)
+            string input = "Rules.ini", output = "LangFS.ini";
+            if (CheckForHelpFlag(args))
             {
-                using(StreamWriter sw = new StreamWriter(File.OpenWrite(arg + ".txt")))
-                {
-                    IniFile iniFile = new IniFile(arg);
-                    var listOfSections = iniFile.GetSections().AsParallel().Where(x => iniFile.GetStringValue(x, "Name", string.Empty) != string.Empty).ToList();
-                    listOfSections.Sort();
-                    foreach (var section in listOfSections)
+                PrintHelp();
+                return;
+            }
+
+            switch (args.Length)
+            {
+                case 0:
+                    PrintHelp();
+                    return;
+                case 1:
+                    input  = args[0];
+                    break;
+                case 2:
+                    input  = args[0];
+                    output = args[1];
+                    break;
+                case 4:
+                    if (!((args[0] == "-i" ||
+                           args[0] == "--input") &&
+                          (args[2] == "-o" ||
+                           args[2] == "--output")))
                     {
-                        sw.WriteLine("[" + section + "]");
-                        sw.WriteLine("Name=" + iniFile.GetStringValue(section, "Name", string.Empty));
-                        sw.WriteLine();
+                        PrintHelp();
+                        return;
                     }
+                    else
+                    {
+                        input  = args[1];
+                        output = args[3];
+                    }
+                    break;
+                default:
+                    PrintHelp();
+                    return;
+            }
+
+            if (input == output)
+            {
+                Console.WriteLine("Output file cannot be the same as the input file!");
+                return;
+            }
+
+            using (StreamWriter sw = new StreamWriter(File.OpenWrite(output)))
+            {
+                IniFile iniFile = new IniFile(input);
+                var listOfSections = iniFile.GetSections()
+                                            .AsParallel()
+                                            .Where(x => iniFile.GetStringValue(x, "Name", string.Empty) != string.Empty)
+                                            .ToList();
+                listOfSections.Sort();
+                foreach (var section in listOfSections)
+                {
+                    sw.WriteLine("[" + section + "]");
+                    sw.WriteLine("Name=" + iniFile.GetStringValue(section, "Name", string.Empty));
+                    sw.WriteLine();
                 }
             }
+        }
+        static void PrintHelp()
+        {
+            Console.WriteLine("LangFS Names Exporter by mah_boi\n" +
+                              "Powered with Rampastring.Tool by Rampastring\n\n" +
+                              
+                              "This utillity export all sections that contains Name key.\n" +
+                              "It must help you with finding and preparing LangFS.ini file\n" +
+                              "for next translation it to any language.\n\n" +
+
+                              "Available flags:\n" +
+                              "\t-h, -help, --help, /?, /h, /help -- prints info about utility\n" +
+                              "\t-i, --input                      -- set path to Rules.ini file\n" +
+                              "\t-o, --output                     -- set path to LangFS.ini file\n\n" +
+
+                              "Arguments example:\n" +
+                              "\tLangFSExporter --help\n" +
+                              "\tLangFSExporter rules.ini\n" +
+                              "\tLangFSExporter rules.ini language.ini\n" +
+                              "\tLangFSExporter -i ..\\src\\rules.ini -o langfs.txt\n" +
+                              "\tLangFSExporter --input src\\rules.ini --output src\\langfs.ini\n");
+        }
+        static bool CheckForHelpFlag(string[] args)
+        {
+            foreach (var flag in args)
+            {
+                switch(flag)
+                {
+                    case "/?":
+                        return true;
+                    case "-h":
+                        return true;
+                    case "/h":
+                        return true;
+                    case "-help":
+                        return true;
+                    case "/help":
+                        return true;
+                    case "--help":
+                        return true;
+                    default:
+                        break;
+                }
+            }
+            return false;
         }
     }
 }
